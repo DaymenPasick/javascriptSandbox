be84390aceab21e18f6f78fe0ba09f673eac9827
Daymens Code <Daymenscode@gmail.com>
2023-12-05 06:29:27 -0500

    woop

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,1 +1,23 @@
-console.log("test")
+
+class Pokemon {
+    constructor(name, type, num){
+        this.name = name;
+        this.type = type;
+        this.num = num;
+    }
+};
+
+//Pokemon objects
+var pikachu = new Pokemon("Pikachu", "Electric", 2)
+var charmander = new Pokemon("Charmander", "Fire", 5)
+
+//Pokemon Array
+var pokemonArray = [];
+pokemonArray.push(pikachu)
+pokemonArray.push(charmander)
+
+
+console.log(pokemonArray)
+//Console log is (2)[Pokemon, Pokemon]
+
+
