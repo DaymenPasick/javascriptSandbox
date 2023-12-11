bb2b819402e5471ff4a5ee1e69dd4e9cb3d315b8
Daymens Code <Daymenscode@gmail.com>
2023-12-11 04:19:39 -0500

    learning prototypes

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,32 +1,49 @@
 
-class Pokemon {
-    constructor(name, type, num){
-        this.name = name;
-        this.type = type;
-        this.num = num;
+// class Pokemon {
+//     constructor(name, type, num){
+//         this.name = name;
+//         this.type = type;
+//         this.num = num;
+//     }
+// };
+
+// //Pokemon objects
+// var pikachu = new Pokemon("Pikachu", "Electric", 2)
+// var charmander = new Pokemon("Charmander", "Fire", 5)
+
+// //Pokemon Array
+// var pokemonArray = [];
+// pokemonArray.push(pikachu)
+// pokemonArray.push(charmander)
+
+
+// console.log(pokemonArray)
+// //Console log is (2)[Pokemon, Pokemon]
+
+
+// var nameEntry = document.getElementById("form")
+// nameEntry.addEventListener('keypress', function(event){
+//     if (event.key === 'Enter'){
+//     event.preventDefault();
+//     var pokeName = document.getElementById("name").value;
+//     console.log(pokeName)
+//     event.currentTarget.value = "";
+//   }
+// })
+
+
+
+
+
+class Person {
+    talk() {
+        return 'Talking'
     }
-};
-
-//Pokemon objects
-var pikachu = new Pokemon("Pikachu", "Electric", 2)
-var charmander = new Pokemon("Charmander", "Fire", 5)
-
-//Pokemon Array
-var pokemonArray = [];
-pokemonArray.push(pikachu)
-pokemonArray.push(charmander)
+}
 
 
-console.log(pokemonArray)
-//Console log is (2)[Pokemon, Pokemon]
+const me = new Person ()
+const you = new Person ()
 
-
-var nameEntry = document.getElementById("form")
-nameEntry.addEventListener('keypress', function(event){
-    if (event.key === 'Enter'){
-    event.preventDefault();
-    var pokeName = document.getElementById("name").value;
-    console.log(pokeName)
-    event.currentTarget.value = "";
-  }
-})
+console.log(Person.prototype);
+// console.log(you.talk());
